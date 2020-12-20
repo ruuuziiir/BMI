922c310b663a27c27e24b403fc1a91cd9ee2094f
7 <ruuuziiir@gmail.com>
2020-12-20 20:16:27 +0800

    first

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    @IBOutlet weak var heightNumber: UILabel!
+    @IBOutlet weak var weightNumber: UILabel!
+    
+    @IBOutlet weak var bmiNumber: UILabel!
+    @IBOutlet weak var commentText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        heightSlider.value = 150
+        heightNumber.text = "150.0"
+        
+        weightSlider.value = 70
+        weightNumber.text = "70.0"
+        
+        commentText.text = ""
     }
 
-
+    @IBAction func heightAction(_ sender: UISlider) {
+        
+        heightSlider.value = sender.value
+        heightNumber.text = String(format: "%.1f", heightSlider.value)
+        
+    }
+    
+    @IBAction func weightAction(_ sender: UISlider) {
+        
+        weightSlider.value = sender.value
+        weightNumber.text = String(format: "%.1f", weightSlider.value)
+    }
+    
+    
+    @IBAction func calculateButton(_ sender: Any) {
+        
+        let a = (weightSlider.value / (heightSlider.value * heightSlider.value)) * 10000
+        
+        bmiNumber.text = String(format: "%.2f", a)
+        
+        if a < 18.5 {
+            commentText.text = "Underweight."
+        }
+        
+        if 18.5 <= a && a < 25 {
+            commentText.text = "Normal Range."
+        }
+        if 25 <= a && a < 30 {
+            commentText.text = "Overweight."
+        }
+        if a >= 30 {
+            commentText.text = "Obese!"
+        }
+        
+    }
 }
 
