fb05db0e290644c531aa251ffcffe206c4b6449d
7 <ruuuziiir@gmail.com>
2020-12-20 18:34:00 +0800

    Initial Commit

---
+++ BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by 7 on 2020/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
